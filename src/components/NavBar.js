5782bd89639ffea5274b416a68860b1c89a3a82f
Tomas Zurita Sorgetti <tomaszuritasorgetti@gmail.com>
2022-10-28 17:29:00 -0300

    preentrega1-zurita

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,19 @@
 import CartWidget from "./CartWidget";
+import logo from "../img/logo.png";
 
 const NavBar= () => {
     return (
         <>
             <header className = "header">
-                <p className = "logo">logo</p>
+                <a href="../App.js"><img src = {logo} className = "logo" alt = "logo"/></a>    
                 <ul className = "navBar">
                     <li><a href="../App.js">Inicio</a></li>
                     <li><a href="../App.js">Productos</a></li>
                     <li><a href="../App.js">Contacto</a></li>
                 </ul>
-                <div className = "cart"><CartWidget/></div>
+                <div className = "cart"><a href = "../App.js"><CartWidget/></a></div>
             </header>
+        
         </>
     )
 };
